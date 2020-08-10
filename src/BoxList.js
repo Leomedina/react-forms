39cc63b87856a2832427a6e5f896a594dffb34bb
Leo <medinayleo@gmail.com>
2020-08-10 16:26:12 -0400

    created basic structure of color box components

---
+++ src/BoxList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import box from './Box'
+
+const boxList = () => (
+  null
+);
+
+export default boxList;
---
+++ src/NewBoxForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const newBoxForm = () => (
+  null
+);
+
+export default newBoxForm;
